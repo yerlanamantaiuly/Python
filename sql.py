c2a9d8684fb5719113e22ee2469580da1d8ce725
Ерлан Амантайулы <ye.amantaiuly@small.kz>
2021-10-12 15:05:35 +0600

    second commit

---
+++ sql.py
@@ -0,0 +1,12 @@
+import pyodbc
+from datetime import datetime
+
+class Sql:
+    def __init__(self, database, server="10.1.16.177"):
+        self.cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                                   "Server="+server+";"
+                                   "Database="+database+";"
+                                   "Trusted_Connection=yes;")
+        self.query = "-- {}\n\n-- Made in Python".format(datetime.now()
+                                                         .strftime("%d/%m/%Y"))
+print("удача")
---
+++ test.py
@@ -0,0 +1,6 @@
+a = [1, 2, 3]
+
+b = list(a)
+a.append(4)
+
+print( b , a)
